2ad30a64a38fa144dcb0921e3656a90f2b28db30
LULU <384668412@qq.com>
2024-04-18 15:11:39 +0800

    [feat] enable kubectl apply -f

--- pkg/kubectl/cmd/applycmd.go
+++ pkg/kubectl/cmd/applycmd.go
@@ -2,9 +2,27 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
+
+type Resource struct {
+	ApiVersion string `yaml:"apiVersion"`
+	Kind       string `yaml:"kind"`
+	Metadata   struct {
+		Name string `yaml:"name"`
+	} `yaml:"metadata"`
+	Spec struct {
+		Replicas int `yaml:"replicas"`
+		Selector struct {
+			MatchLabels struct {
+				App string `yaml:"nginx"`
+			} `yaml:"matchLabels"`
+		} `yaml:"spec"`
+	}
+}
 
 func ApplyCmd() *cobra.Command {
 
@@ -21,10 +39,62 @@ func ApplyCmd() *cobra.Command {
 
 func applyCmdHandler(cmd *cobra.Command, args []string) {
 	path, err := cmd.Flags().GetString("file")
-
 	if err != nil {
+		fmt.Println("Error getting flags:", err)
 		return
 	}
 
-	fmt.Println("the file you specified is:", path)
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+
+	var resource Resource
+	err = decoder.Decode(&resource)
+	if err != nil {
+		fmt.Println("Error decoding yaml:", err)
+	}
+
+	fmt.Println("Decode yaml successfully, resource:", resource)
+
+	switch resource.Kind {
+	case "Pod":
+		applyPodHandler(resource)
+	case "Service":
+		applyServiceHandler(resource)
+	case "Deployment":
+		applyDeploymentHandler(resource)
+	case "ReplicaSet":
+		applyReplicaSetHandler(resource)
+	case "StatefulSet":
+		applyStatefulSetHandler(resource)
+	default:
+		fmt.Println("Unknown resource kind")
+	}
+
+}
+
+func applyPodHandler(resource Resource) {
+	fmt.Println("creating or updating pod")
+}
+
+func applyServiceHandler(resource Resource) {
+	fmt.Println("creating or updating service")
+}
+
+func applyDeploymentHandler(resource Resource) {
+	fmt.Println("creating or updating deployment")
+}
+
+func applyReplicaSetHandler(resource Resource) {
+	fmt.Println("creating or updating replicaset")
+}
+
+func applyStatefulSetHandler(resource Resource) {
+	fmt.Println("creating or updating statefulset")
 }
