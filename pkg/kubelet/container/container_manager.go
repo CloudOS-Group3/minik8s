5acc6014c1cb8196255819a2dc67a5ec4bd6c9be
Xinmin Du <10803082+doris-xm@user.noreply.gitee.com>
2024-04-21 16:24:26 +0800

    feat: start, stop, remove container

--- pkg/kubelet/container/container_manager.go
+++ pkg/kubelet/container/container_manager.go
@@ -11,6 +11,7 @@ import (
 	"minik8s/pkg/api"
 	"minik8s/pkg/kubelet/image"
 	"minik8s/pkg/util"
+	"syscall"
 	"time"
 )
 
@@ -64,52 +65,144 @@ func (cm *ContainerManager) CreateContainer(config api.Container, namespace stri
 
 }
 
-func (*ContainerManager) StartContainerById(container_id string, namespace string) bool {
+func (*ContainerManager) GetContainerById(container_id string, namespace string) containerd.Container {
 	client, err := util.CreateClient()
 	if err != nil {
 		log.Printf("Failed to create containerd client: %v", err.Error())
-		return false
+		return nil
 	}
 	ctx := namespaces.WithNamespace(context.Background(), namespace)
 	container_, err := client.LoadContainer(ctx, container_id)
 	if err != nil {
 		log.Printf("Failed to load container %s: %v", container_id, err.Error())
-		return false
+		return nil
 	}
-
-	// check if already started
-	_, err = container_.Task(ctx, nil)
-	if err == nil {
-		log.Printf("Container %s already started", container_id)
-		return true
-	}
-
-	task, err := container_.NewTask(ctx, cio.NewCreator(cio.WithStdio))
-	if err != nil {
-		log.Printf("Failed to create task for container %s: %v", container_id, err.Error())
-		return false
-	}
-	err = task.Start(ctx)
-	if err != nil {
-		log.Printf("Failed to start task for container %s: %v", container_id, err)
-		return false
-	}
-	log.Printf("Container %s: %s started", namespace, container_id)
-	return true
+	return container_
 }
 
-func (*ContainerManager) StartContainer(container containerd.Container) bool {
-	ctx := context.Background()
-	task, err := container.NewTask(ctx, nil)
+func (*ContainerManager) StartContainer(container containerd.Container, ctx context.Context) bool {
+	// check if already started
+	tasks, _ := container.Task(ctx, nil)
+	if tasks != nil {
+		status, _ := tasks.Status(ctx)
+		//log.Printf("Container %s status: %v", container.ID(), status.Status)
+		if status.Status == containerd.Running {
+			log.Printf("Container %s already started", container.ID())
+			return true
+		}
+		tasks.Delete(ctx, containerd.WithProcessKill)
+
+		//// why it can't start a stopped task?
+		//err :=  tasks.Start(ctx)
+		//if err != nil {
+		//	log.Printf("Failed to start task for container %s: %v", container.ID(), err.Error())
+		//	return false
+		//}
+		//return true
+	}
+
+	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
 	if err != nil {
 		log.Printf("Failed to create task for container %s: %v", container.ID(), err.Error())
 		return false
 	}
 	err = task.Start(ctx)
 	if err != nil {
-		log.Printf("Failed to start task for container %s: %v", container.ID(), err)
+		log.Printf("Failed to start task for container %s: %v", container.ID(), err.Error())
 		return false
 	}
 	log.Printf("Container %s started", container.ID())
+	return true
+}
+
+func (*ContainerManager) StopContainer(container containerd.Container, ctx context.Context) bool {
+	// search for task
+	task, err := container.Task(ctx, nil)
+	if err != nil {
+		log.Printf("Failed to get task for container %s: %v", container.ID(), err.Error())
+		return false
+	}
+
+	// check if already stopped
+	status, err := task.Status(ctx)
+	if err == nil && status.Status == containerd.Stopped {
+		log.Printf("Container %s already stopped", container.ID())
+		return true
+	}
+
+	// kill task
+	err = task.Kill(ctx, syscall.SIGTERM)
+	if err != nil {
+		log.Printf("Failed to kill task for container %s: %v", container.ID(), err)
+		return false
+	}
+
+	// wait for task to exit
+	exitStatusC, err := task.Wait(ctx)
+	if err != nil {
+		log.Printf("Failed to wait task for container %s: %v", container.ID(), err.Error())
+		return false
+	}
+	select {
+	case <-exitStatusC:
+		break
+	case <-time.After(30 * time.Second):
+		log.Printf("Failed to wait task for container %s: timeout", container.ID())
+	}
+	log.Printf("Container %s stopped", container.ID())
+	return true
+}
+
+func (*ContainerManager) RemoveContainer(container containerd.Container, ctx context.Context) bool {
+	// search for task
+	task, err := container.Task(ctx, nil)
+	if err != nil {
+		log.Printf("Failed to get task for container %s: %v", container.ID(), err.Error())
+		return false
+	}
+	// check status
+	status, err := task.Status(ctx)
+	if err != nil {
+		log.Printf("Failed to get task status for container %s: %v", container.ID(), err.Error())
+		return false
+	}
+
+	// stop task if running
+	if status.Status == containerd.Running {
+		err = task.Kill(ctx, syscall.SIGTERM)
+		if err != nil {
+			log.Printf("Failed to kill task for container %s: %v", container.ID(), err.Error())
+			return false
+		}
+
+		// wait for task to exit
+		exitStatusC, err := task.Wait(ctx)
+		if err != nil {
+			log.Printf("Failed to wait task for container %s: %v", container.ID(), err.Error())
+			return false
+		}
+		select {
+		case <-exitStatusC:
+			break
+		case <-time.After(30 * time.Second):
+			log.Printf("Failed to wait task for container %s: timeout", container.ID())
+			return false
+		}
+
+	}
+	// delete task
+	_, err = task.Delete(ctx)
+	if err != nil {
+		log.Printf("Failed to remove task for container %s: %v", container.ID(), err.Error())
+		return false
+	}
+
+	// remove container
+	err = container.Delete(ctx, containerd.WithSnapshotCleanup)
+	if err != nil {
+		log.Printf("Failed to remove container %s: %v", container.ID(), err.Error())
+		return false
+	}
+	log.Printf("Container %s removed", container.ID())
 	return true
 }
