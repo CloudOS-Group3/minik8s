76f2677ffb4846e176cb7c345d8b3edc98230727
LULU <384668412@qq.com>
2024-05-01 19:07:31 +0800

    feat: add etcd prefix config and use it in apiserver

--- pkg/apiserver/handlers/handlers.go
+++ pkg/apiserver/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"minik8s/pkg/api"
 	"minik8s/pkg/apiserver/config"
+	"minik8s/pkg/apiserver/serverconfig"
 	"minik8s/pkg/etcd"
 	"minik8s/pkg/kafka"
 	"minik8s/util/log"
@@ -14,11 +15,12 @@ import (
 
 // all of the following handlers need to call etcd
 
-
 var publisher kafka.Publisher
+var etcdClient etcd.Store
 
 func init() {
 	publisher = *kafka.NewPublisher([]string{"localhost:9092"})
+	etcdClient = *etcd.NewStore()
 }
 
 func GetNodes(context *gin.Context) {
@@ -27,6 +29,29 @@ func GetNodes(context *gin.Context) {
 
 func AddNode(context *gin.Context) {
 	log.Info("received add node request")
+	newNode := &api.Node{}
+	if err := context.ShouldBind(newNode); err != nil {
+		log.Error("decode node failed")
+		context.JSON(http.StatusOK, gin.H{
+			"status": "wrong",
+		})
+		return
+	}
+
+	nodeByteArray, err := json.Marshal(newNode)
+
+	if err != nil {
+		log.Error("error marshal new node")
+		return
+	}
+
+	URL := serverconfig.EtcdNodePath + newNode.Name
+	etcdClient.PutEtcdPair(URL, string(nodeByteArray))
+
+	context.JSON(http.StatusOK, gin.H{
+		"statas": "ok",
+	})
+
 }
 
 func GetNode(context *gin.Context) {
@@ -38,17 +63,55 @@ func GetNode(context *gin.Context) {
 		return
 	}
 
-	// todo: should query from etcd here
-	// and we dont have node object yet...
+	nodeJson := etcdClient.GetEtcdPair(name)
 
+	var node api.Node
+	json.Unmarshal([]byte(nodeJson), node)
+
+	log.Info("node info: %+v", node)
+
+	context.JSON(http.StatusOK, gin.H{
+		"data": node,
+	})
 }
 
 func DeleteNode(context *gin.Context) {
 	log.Info("received delete node request")
+	name := context.Param(config.NameParam)
+
+	if name == "" {
+		log.Error("node name empty")
+		return
+	}
+	
+	URL := serverconfig.EtcdNodePath + name
+	etcdClient.DeleteEtcdPair(URL)
 }
 
 func UpdateNode(context *gin.Context) {
 	log.Info("received udpate node request")
+
+	newNode := &api.Node{}
+	if err := context.ShouldBind(newNode); err != nil {
+		log.Error("decode node failed")
+		context.JSON(http.StatusBadRequest, gin.H{
+			"status": "wrong",
+		})
+		return
+	}
+
+	nodeByteArray, err := json.Marshal(newNode)
+
+	if err != nil {
+		log.Error("error marshal newNode to json string")
+	}
+
+	URL := serverconfig.EtcdNodePath + newNode.Name
+	etcdClient.PutEtcdPair(URL, string(nodeByteArray))
+
+	context.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
 }
 
 func GetPods(context *gin.Context) {
@@ -56,8 +119,6 @@ func GetPods(context *gin.Context) {
 }
 
 func AddPod(context *gin.Context) {
-	// for the time being we dont interact with etcd
-	// instead we directly communicate with pod manager
 	log.Info("received add pod request")
 
 	newPod := &api.Pod{}
@@ -70,8 +131,7 @@ func AddPod(context *gin.Context) {
 	log.Debug("new pod is: %+v", newPod)
 
 	// need to interact with etcd
-	etcd.EtcdStore.PutPod(*newPod)
-
+	etcdClient.PutPod(*newPod)
 
 	podByteArray, err := json.Marshal(newPod)
 
@@ -79,9 +139,9 @@ func AddPod(context *gin.Context) {
 		log.Error("Error: json marshal failed")
 		return
 	}
-	
+
 	publisher.Publish("pod", string(podByteArray))
-	
+
 }
 
 func GetPod(context *gin.Context) {
@@ -110,8 +170,22 @@ func GetPod(context *gin.Context) {
 
 func UpdatePod(context *gin.Context) {
 	log.Info("received update pod request")
+
+	newPod := &api.Pod{}
+	if err := context.ShouldBind(newPod); err != nil {
+		log.Error("error decode new pod")
+		return
+	}
+
+	// todo: should use update pod
+	etcdClient.PutPod(*newPod)
+
 }
 
 func DeletePod(context *gin.Context) {
 	log.Info("received delete pod request")
+
+	name := context.Param(config.NameParam)
+	namespace := context.Param(config.NamespaceParam)
+	etcdClient.DeletePod(namespace, name)
 }
