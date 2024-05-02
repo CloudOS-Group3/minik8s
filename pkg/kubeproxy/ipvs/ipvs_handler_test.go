440ed41948defd7afff05c30e81bfab171fe4b08
Xinmin Du <10803082+doris-xm@user.noreply.gitee.com>
2024-05-02 21:27:08 +0800

    fea: add service (ipvs)

---
+++ pkg/kubeproxy/ipvs/ipvs_handler_test.go
@@ -0,0 +1,44 @@
+package ipvs
+
+import (
+	"minik8s/pkg/api"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestIpvsHandler_AddService(t *testing.T) {
+	ipvsHandler := IpvsHandler{}
+	service := api.Service{
+		APIVersion: "v1",
+		Kind:       "Service",
+		Metadata: api.ObjectMeta{
+			Name: "nginx",
+		},
+		Spec: api.ServiceSpec{
+			Type: "ClusterIP",
+			Ports: []api.ServicePort{
+				{
+					Port:       81,
+					TargetPort: 8080,
+					Protocol:   "TCP",
+					Name:       "http",
+				},
+			},
+		},
+	}
+	err := ipvsHandler.AddService(&service)
+	if err != nil {
+		t.Errorf("AddService() error = %v", err)
+	}
+
+	// 检查 IPVS 是否包含添加的 TCP 服务
+	output, err := exec.Command("ipvsadm", "-Ln").CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to run ipvsadm: %v", err)
+	}
+
+	if !strings.Contains(string(output), "TCP  0.0.0.0:81 rr") {
+		t.Error("Expected TCP 0.0.0.0:81 rr in ipvsadm output, but not found")
+	}
+}
