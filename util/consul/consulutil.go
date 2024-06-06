c9bf277872f455cd427dd60485f3a7bb041178e9
Chen Yinghao <1216334362@qq.com>
2024-06-07 00:54:35 +0800

    fix: enable Prometheus

--- util/consul/consulutil.go
+++ util/consul/consulutil.go
@@ -5,21 +5,20 @@ import "github.com/hashicorp/consul/api"
 var client *api.Client
 
 func init() {
-	//config := api.DefaultConfig()
-	//config.Address = "192.168.3.8:8500"
-	//client, _ = api.NewClient(config)
+	config := api.DefaultConfig()
+	config.Address = "192.168.3.8:8500"
+	client, _ = api.NewClient(config)
 }
 
 func RegisterService(ID string, name string, addr string, port int) {
-
-	//registration := new(api.AgentServiceRegistration)
-	//registration.ID = ID
-	//registration.Name = name
-	//registration.Address = addr
-	//registration.Port = port
-	//_ = client.Agent().ServiceRegister(registration)
+	registration := new(api.AgentServiceRegistration)
+	registration.ID = ID
+	registration.Name = name
+	registration.Address = addr
+	registration.Port = port
+	_ = client.Agent().ServiceRegister(registration)
 }
 
 func DeRegisterService(ID string) {
-	//_ = client.Agent().ServiceDeregister(ID)
+	_ = client.Agent().ServiceDeregister(ID)
 }
